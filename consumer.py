71d7216c23e193fd8e2732a74f32def26a867444
tejas-goyal <83608316+tejas-goyal@users.noreply.github.com>
2023-08-08 22:12:55 +0530

    from ubuntu run

--- consumer.py
+++ consumer.py
@@ -1,83 +1,38 @@
+'''from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    'reddit-comments',
+     bootstrap_servers=['localhost:9092'],
+     value_deserializer=lambda x: json.loads(x.decode('utf-8')))
+
+for message in consumer:
+    print(message.value)'''
+    
+import os
+os.environ['SPARK_HOME'] = '/opt/spark'    
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-import mysql.connector
+from pyspark.sql.functions import window, count
 
-# Set up Spark session
-spark = SparkSession.builder.appName("reddit-streaming-consumer").getOrCreate()
+spark = SparkSession.builder.appName('RedditComments').getOrCreate()
 
-# Set up StreamingContext with batch interval of 5 seconds
-ssc = StreamingContext(spark.sparkContext, 5)
+# Define the input stream from Kafka
+df = spark.readStream.format('kafka') \
+    .option('kafka.bootstrap.servers', 'localhost:9092') \
+    .option('subscribe', 'reddit-comments') \
+    .option('startingOffsets', 'earliest') \
+    .load()
 
-# Set up Kafka parameters
-kafkaParams = {
-    "bootstrap.servers": "localhost:9092",
-    "group.id": "reddit-group",
-    "auto.offset.reset": "largest"
-}
+# Parse the message value as JSON
+df = df.selectExpr("CAST(value AS STRING)")
 
-# Set up list of topics to subscribe to
-topics = ["reddit_stream"]
+# Define a sliding window of 1 minute and count the number of comments in each window
+windowedCounts = df.groupBy(window(df.timestamp, "1 minute")).agg(count('*').alias('comment_count'))
 
-# Define schema for incoming JSON data
-jsonSchema = StructType([
-    StructField("id", StringType(), True),
-    StructField("title", StringType(), True),
-    StructField("score", IntegerType(), True),
-    StructField("num_comments", IntegerType(), True),
-    StructField("created_utc", IntegerType(), True),
-    StructField("subreddit", StringType(), True),
-    StructField("over_18", BooleanType(), True)
-])
+# Start the query and write the results to the console
+query = windowedCounts.writeStream.outputMode('complete').format('console').start()
 
-# Create function to save data to MySQL database
-'''def save_to_mysql(rdd):
-    # Create MySQL connection
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="reddit"
-    )
-    mycursor = mydb.cursor()
+# Wait for the query to terminate
+query.awaitTermination()
 
-    # Loop through RDD and insert data into MySQL table
-    for row in rdd.collect():
-        sql = "INSERT INTO reddit_posts (id, title, score, num_comments, created_utc, subreddit, over_18) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = (row["id"], row["title"], row["score"], row["num_comments"], row["created_utc"], row["subreddit"], row["over_18"])
-        mycursor.execute(sql, val)
 
-    # Commit changes and close connection
-    mydb.commit()
-    mycursor.close()'''
-
-# Create Kafka stream
-stream = KafkaUtils.createDirectStream(ssc, topics, kafkaParams)
-
-# Parse incoming JSON data and select relevant columns
-parsed_stream = stream.map(lambda x: x[1]).\
-    select(from_json(col("value").cast("string"), jsonSchema).alias("json")).\
-    select("json.*")
-
-# Count number of posts within each window
-posts_count = parsed_stream.count()
-
-# Group posts by subreddit and count number of posts within each subreddit in each window
-subreddit_counts = parsed_stream.groupBy("subreddit").count()
-
-# Min and max scores of posts within each window
-min_max_scores = parsed_stream.selectExpr("min(score) as min_score", "max(score) as max_score")
-
-# Print results to console
-posts_count.pprint()
-subreddit_counts.pprint()
-min_max_scores.pprint()
-
-# Save some data to MySQL database
-#parsed_stream.foreachRDD(lambda rdd: save_to_mysql(rdd))
-
-# Start the streaming context
-ssc.start()
-ssc.awaitTermination()
--- producer.py
+++ producer.py
@@ -1,64 +1,25 @@
 import praw
+from kafka import KafkaProducer
 import json
 import time
-from kafka import KafkaProducer
-from pyspark.sql import SparkSession
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-import mysql.connector
 
-# Reddit API credentials
+# Set up Reddit API credentials
 reddit = praw.Reddit(client_id='Xhm4uV7R-SQBWfd5jXlmkQ',
                      client_secret='jnVVQeeqJhNC4gOqDpg2hmDrrOCM2A',
-                     user_agent='reddit streaming data analysis',
-                     username='tejas_goyal',
-                     password='6303@Anriya')
+                     redirect_uri='http://localhost:8080',
+                     user_agent='myBot/0.0.1')
 
-# Kafka Producer configuration
+
+
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda m: json.dumps(m).encode('ascii'))
+                         value_serializer=lambda x: 
+                         json.dumps(x).encode('utf-8'))
 
-# Kafka Consumer configuration
-kafkaParams = {"metadata.broker.list": "localhost:9092"}
-topics = ['reddit_stream']
+subreddit = reddit.subreddit('all')
+comments = subreddit.stream.comments(skip_existing=True)
 
-# Spark Streaming configuration
-ssc = StreamingContext(SparkSession.builder.appName("RedditStream").getOrCreate(), 10)
-kafkaStream = KafkaUtils.createDirectStream(ssc, topics, kafkaParams)
+for comment in comments:
+    producer.send('reddit-comments', value=comment.body)
+    print(comment.body)
+    time.sleep(1)
 
-# Function to process and save data to MariaDB
-def process_data(rdd):
-    if not rdd.isEmpty():
-        # Convert RDD to DataFrame
-        spark = SparkSession.builder.appName("RedditStream").getOrCreate()
-        df = spark.read.json(rdd)
-
-        '''# Create connection to MariaDB
-        cnx = mysql.connector.connect(user='<db_use', password='<db_password>',
-                                       host='<db_host>', database='<db_name>')
-        cursor = cnx.cursor()
-
-        # Insert data into the movies table
-        for row in df.rdd.collect():
-            sql = "INSERT INTO movies (title, subreddit, score) VALUES (%s, %s, %s)"
-            val = (row['title'], row['subreddit'], row['score'])
-            cursor.execute(sql, val)
-        cnx.commit()
-
-        # Close MariaDB connection
-        cursor.close()
-        cnx.close()'''
-
-# Function to extract required data from Reddit stream and send it to Kafka topic
-def send_to_kafka():
-    for comment in reddit.subreddit('all').stream.comments():
-        producer.send('reddit_stream', {'title': comment.submission.title, 
-                                        'subreddit': comment.submission.subreddit.display_name, 
-                                        'score': comment.submission.score})
-
-# Start the Spark Streaming Context
-kafkaStream.foreachRDD(process_data)
-ssc.start()
-
-# Start the Kafka Producer
-send_to_kafka()
